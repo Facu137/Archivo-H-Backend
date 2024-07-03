2d9d8e2d48ab28c01211584639295db15510a0e3
Niicor <niicorivero@gmail.com>
2024-07-03 12:31:24 -0300

    Agregar ruta de busqueda

--- src/app.js
+++ src/app.js
@@ -1,11 +1,9 @@
 import express from "express";
 import cors from "cors";
 import path from "path";
-
 import { config } from "dotenv";
 import authRoutes from "./routes/authRoutes.js";
 import fileRoutes from "./routes/fileRoutes.js";
-
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -19,10 +17,8 @@ app.use(express.json()); // para analizar el body de las peticiones
 
 // Rutas
 app.use("/auth", authRoutes); // ruta de autenticación
-
 app.use("/api", fileRoutes); // ruta para subir archivos
 app.use("/uploads", express.static(path.join(process.cwd(), "uploads"))); // ruta para subir archivos
-
 
 // Iniciar servidor
 app.listen(port, () => {
