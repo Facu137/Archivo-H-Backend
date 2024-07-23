9f42ef29483516155998dad7c637fb80b3de0751
Niicor <niicorivero@gmail.com>
2024-07-23 12:47:59 -0300

    Separacion del fileController

---
+++ src/controllers/fileController/getFilesByUser.js
@@ -0,0 +1,25 @@
+import pool from "../../config/db.js";
+
+const getFileUrl = (filename) => {
+  return `http://localhost:3000/uploads/${filename}`;
+};
+
+export const getFilesByUser = async (req, res) => {
+  const { userId } = req.params;
+  try {
+    const [rows] = await pool.query(`
+      SELECT d.*, i.url, i.tipo_imagen 
+      FROM documentos d 
+      LEFT JOIN imagenes i ON d.id = i.documento_id
+      WHERE d.creador_id = ?
+    `, [userId]);
+    const filesWithUrl = rows.map(file => ({
+      ...file,
+      url: file.url ? getFileUrl(file.url) : null
+    }));
+    res.json(filesWithUrl);
+  } catch (error) {
+    console.error("Error al obtener documentos por usuario:", error);
+    res.status(500).send("Error al obtener documentos por usuario");
+  }
+};
