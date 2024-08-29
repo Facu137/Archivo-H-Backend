0da40d95bfd9f55c361afca9345de7878be1466c
Niicor <niicorivero@gmail.com>
2024-08-28 21:21:42 -0300

    getDeletedFiles funcionando

--- src/controllers/documentController/getDeletedFiles.js
+++ src/controllers/documentController/getDeletedFiles.js
@@ -10,56 +10,56 @@ async function getDeletedDocuments(req, res) {
     // Consulta SQL para obtener documentos eliminados
     const sql = `
       SELECT DISTINCT
-  d.id AS documento_id,
-  d.tipo_documento,
-  d.anio,
-  d.mes,
-  d.dia,
-  d.caratula_asunto_extracto,
-  d.tema,
-  d.folios,
-  d.fecha_marcado_eliminacion,
-  GROUP_CONCAT(DISTINCT CONCAT(dp.rol, ':', pa.nombre, ' (', pa.tipo, ')') SEPARATOR '; ') AS personas,
-  GROUP_CONCAT(DISTINCT dep.nombre SEPARATOR ', ') AS departamentos,
-  GROUP_CONCAT(DISTINCT i.url SEPARATOR ', ') AS imagenes_url,
-  l.numero AS legajo_numero,
-  e.numero AS expediente_numero,
-  m.lugar AS mensura_lugar,
-  m.propiedad AS mensura_propiedad,
-  n.registro AS notarial_registro,
-  n.protocolo AS notarial_protocolo,
-  n.mes_inicio AS notarial_mes_inicio,
-  n.mes_fin AS notarial_mes_fin,
-  n.escritura_nro AS notarial_escritura_nro,
-  n.negocio_juridico AS notarial_negocio_juridico,
-  pu.nombre AS usuario_eliminacion_nombre,
-  pu.apellido AS usuario_eliminacion_apellido,
-  ac.fecha_hora AS fecha_eliminacion
-FROM
-  auditoria_cambios ac
-JOIN documentos d ON ac.id_registro_afectado = d.id
-LEFT JOIN documentos_departamentos dd ON d.id = dd.documento_id
-LEFT JOIN departamentos dep ON dd.departamento_id = dep.id
-LEFT JOIN documentos_personas dp ON d.id = dp.documento_id
-LEFT JOIN personas_archivo pa ON dp.persona_id = pa.id
-LEFT JOIN imagenes i ON d.id = i.documento_id
-LEFT JOIN expedientes e ON d.expediente_id = e.id
-LEFT JOIN legajos l ON e.legajo_id = l.id
-LEFT JOIN mensura m ON d.id = m.id
-LEFT JOIN notarial n ON d.id = n.id
-LEFT JOIN personas_usuarios pu ON ac.usuario_id = pu.id
-WHERE
-  ac.tabla_afectada = 'documentos'
-  AND ac.columna_modificada = 'esta_eliminado'
-  AND ac.valor_nuevo = '1'
-  AND d.esta_eliminado = 1
-  AND d.fecha_marcado_eliminacion IS NOT NULL
-  AND DATEDIFF(CURDATE(), d.fecha_marcado_eliminacion) <= 180
-GROUP BY 
-  d.id, 
-  pu.nombre, 
-  pu.apellido 
-ORDER BY ac.fecha_hora DESC;
+        d.id AS documento_id,
+        d.tipo_documento,
+        d.anio,
+        d.mes,
+        d.dia,
+        d.caratula_asunto_extracto,
+        d.tema,
+        d.folios,
+        d.fecha_marcado_eliminacion,
+        GROUP_CONCAT(DISTINCT CONCAT(dp.rol, ':', pa.nombre, ' (', pa.tipo, ')') SEPARATOR '; ') AS personas,
+        GROUP_CONCAT(DISTINCT dep.nombre SEPARATOR ', ') AS departamentos,
+        GROUP_CONCAT(DISTINCT i.url SEPARATOR ', ') AS imagenes_url,
+        l.numero AS legajo_numero,
+        e.numero AS expediente_numero,
+        m.lugar AS mensura_lugar,
+        m.propiedad AS mensura_propiedad,
+        n.registro AS notarial_registro,
+        n.protocolo AS notarial_protocolo,
+        n.mes_inicio AS notarial_mes_inicio,
+        n.mes_fin AS notarial_mes_fin,
+        n.escritura_nro AS notarial_escritura_nro,
+        n.negocio_juridico AS notarial_negocio_juridico,
+        pu.nombre AS usuario_eliminacion_nombre,
+        pu.apellido AS usuario_eliminacion_apellido,
+        MAX(ac.fecha_hora) AS fecha_eliminacion  -- Se eliminó el comentario
+      FROM
+        auditoria_cambios ac
+      JOIN documentos d ON ac.id_registro_afectado = d.id
+      LEFT JOIN documentos_departamentos dd ON d.id = dd.documento_id
+      LEFT JOIN departamentos dep ON dd.departamento_id = dep.id
+      LEFT JOIN documentos_personas dp ON d.id = dp.documento_id
+      LEFT JOIN personas_archivo pa ON dp.persona_id = pa.id
+      LEFT JOIN imagenes i ON d.id = i.documento_id
+      LEFT JOIN expedientes e ON d.expediente_id = e.id
+      LEFT JOIN legajos l ON e.legajo_id = l.id
+      LEFT JOIN mensura m ON d.id = m.id
+      LEFT JOIN notarial n ON d.id = n.id
+      LEFT JOIN personas_usuarios pu ON ac.usuario_id = pu.id
+      WHERE
+        ac.tabla_afectada = 'documentos'
+        AND ac.columna_modificada = 'esta_eliminado'
+        AND ac.valor_nuevo = '1'
+        AND d.esta_eliminado = 1
+        AND d.fecha_marcado_eliminacion IS NOT NULL
+        AND DATEDIFF(CURDATE(), d.fecha_marcado_eliminacion) <= 180
+      GROUP BY 
+        d.id, 
+        pu.nombre, 
+        pu.apellido 
+      ORDER BY fecha_eliminacion DESC;
     `
 
     // Ejecutar la consulta
